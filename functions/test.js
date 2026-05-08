a9e98b6b4c047fe20513431f96a2ea8a2820189c
sqdft <2495997251@qq.com>
2026-05-08 10:26:29 +0800

    clean: remove all functions, will re-add

--- functions/test.js
+++
@@ -1,6 +0,0 @@
-export async function onRequest(context) {
-  return new Response(JSON.stringify({ ok: true, message: 'proxy function is working' }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json', 'X-Proxy': 'true' },
-  });
-}
